8ff4af864fff7ee5c24f20ed948e260dab923831
mzf11125 <77999453+mzf11125@users.noreply.github.com>
2024-05-17 01:24:43 +0700

    update homepage

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,19 @@
 import Image from "next/image";
 import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <Link href="/userPage">User Page</Link>
+    <main
+      style={{
+        backgroundColor: "#00008B",
+        animation: "colorChange 5s infinite",
+      }}
+      className="flex min-h-screen flex-col items-center justify-center p-48 h-screen"
+    >
+      <div className="flex flex-col space-y-8">
+        <Button className="text-lg py-8 px-16">Sign Up</Button>
+        <Button className="text-lg py-8 px-16">Login</Button>
       </div>
     </main>
   );
