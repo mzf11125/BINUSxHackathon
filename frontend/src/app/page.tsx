a5135f2e959d1a1bc71208eff6c231ebc611016b
mzf11125 <77999453+mzf11125@users.noreply.github.com>
2024-05-17 01:43:47 +0700

    update

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,16 +1,26 @@
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 export default function Home() {
   return (
     <main
       style={{
-        backgroundColor: "#00008B",
+        backgroundColor: "#4B0082",
         animation: "colorChange 5s infinite",
       }}
       className="flex min-h-screen flex-col items-center justify-center p-48 h-screen"
     >
+      <h1 className="text-6xl font-bold text-white mb-8">AccessAble</h1>
+      <p className="text-white text-lg mb-8">
+        AccessAble is a platform dedicated to empowering individuals with
+        disabilities by connecting them with job opportunities and providing
+        resources to enhance their skills. Our mission is to level the playing
+        field and create an inclusive workforce where everyone has the chance to
+        succeed.
+      </p>
       <div className="flex flex-col space-y-8">
         <Button className="text-lg py-8 px-16">Sign Up</Button>
         <Button className="text-lg py-8 px-16">Login</Button>
