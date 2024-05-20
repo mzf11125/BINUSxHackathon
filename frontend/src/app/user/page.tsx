bbb09a5701d7aee608234ccc1c9f91239a4a0f57
Kai Folf <git@fcd.im>
2024-05-20 02:22:50 +0000

    feat: make user page only accessible to authenticated user

--- frontend/src/app/user/page.tsx
+++ frontend/src/app/user/page.tsx
@@ -1,10 +1,23 @@
 import React from "react";
 import Link from "next/link";
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
 
-const userPage = () => {
+const userPage = async () => {
+  const supabase = createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    return redirect("/login");
+  }
+
   return (
     <div>
-      <h1>Hello world</h1>
+      <h1>User Page</h1>
+      <p>If you can see this, you are logged in!</p>
       <Link href="/page2">Page 2</Link>
     </div>
   );
